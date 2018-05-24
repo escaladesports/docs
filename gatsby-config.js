60ed31eb40229109244ea3ad0764800e16397497
Kennedy Rose <kennedy@kennedyrose.com>
2018-05-24 16:16:53 -0500

    Logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,6 +49,19 @@ module.exports = {
 					'gatsby-remark-prismjs',
 					'gatsby-remark-copy-linked-files',
 					'gatsby-remark-smartypants',
+					{
+						resolve: `gatsby-transformer-remark`,
+						options: {
+							plugins: [
+								{
+									resolve: "gatsby-remark-external-links",
+									options: {
+										target: "_blank",
+									}
+								}
+							]
+						}
+					},
 				],
 			},
 		},
