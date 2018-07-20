38f7d7700554963b35efe10d04eca0495bfc39d0
Kennedy Rose <kennedyianrose@gmail.com>
2018-07-20 16:05:16 -0500

    Edit button

--- src/templates/default.js
+++ src/templates/default.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import { Helmet } from 'react-helmet'
 import Link from 'gatsby-link'
+import pencil from '../img/pencil.svg'
 
 class DocsTemplate extends React.Component{
 	render(){
@@ -9,6 +10,7 @@ class DocsTemplate extends React.Component{
 		const fm = content.frontmatter
 		const { next, previous } = this.props.pathContext
 		const title = fm.title ? fm.title : content.fields.title
+		console.log(this.props)
 		return(
 			<Fragment>
 				<Helmet>
@@ -17,6 +19,14 @@ class DocsTemplate extends React.Component{
 				</Helmet>
 				<section>
 					<div dangerouslySetInnerHTML={{ __html: content.html }} />
+					<div className='edit'>
+						<a
+							href='https://github.com/escaladesports/docs.escaladesports.com/edit/master/docs/10-browser-support.md'
+							target='_blank'
+						>
+							<img src={pencil} className='pencil' /> Edit on GitHub
+						</a>
+					</div>
 					<div className='prevNext'>
 						{previous &&
 							<div className='prev'>
@@ -34,6 +44,24 @@ class DocsTemplate extends React.Component{
 					@import 'src/css';
 					.prev, .next{
 						margin-top: 40px;
+					}
+					.edit{
+						opacity: .6;
+						font-size: .9em;
+						margin-top: 40px;
+						text-align: right;
+						a{
+							color: var(--colorSecondary);
+							text-decoration: none;
+						}
+						:hover{
+							opacity: 1;
+						}
+					}
+					.pencil{
+						position: relative;
+						top: 2px;
+						margin-right: 2px;
 					}
 					@media(min-width:1000px){
 						.prevNext{
